f7e0f60b7311612bfd705a650926448c8c1013d6
Harichandan Pulagam <harichandan@zoho.com>
2026-04-27 16:25:44 -0700

    keyboard shortcuts

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,6 +2,7 @@ use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
+use tauri::Manager;
 use chrono::{DateTime, Utc};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
